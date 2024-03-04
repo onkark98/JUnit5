4718ba6b7088540d0a0621edf5c494f8d8c72eed
onkark98 <onkark98@gmail.com>
2024-03-04 17:13:06 +0530

    Adding @ BeforeEach Lifecycle hook

--- junit-5-basics/src/test/java/io/javabrains/MathUtilsTest.java
+++ junit-5-basics/src/test/java/io/javabrains/MathUtilsTest.java
@@ -2,22 +2,34 @@ package io.javabrains;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class MathUtilsTest {
 
+	private MathUtils mathUtils;
+
+	@BeforeEach
+	void init()
+	{
+		mathUtils = new MathUtils();
+	}
+
 	@Test
 	void testAdd() {
-		MathUtils mathUtils = new MathUtils();
 		int expected = 2;
 		int actual = mathUtils.add(1, 1);
 		assertEquals(expected, actual);	
 	}
 	
 	@Test
+	void testDivide()
+	{
+		assertThrows(ArithmeticException.class, () -> mathUtils.divide(1, 0), "Divide by zero should throw");
+	}
+	@Test
 	void testComputeCircleRadius()
 	{
-		MathUtils mathUtils = new MathUtils();
 		assertEquals(314.1592653589793, mathUtils.computeCircleArea(10), "Should return right circle area");
 	}
 	
