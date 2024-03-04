b789c8868d21b9b59921816cc4d86008c6856d0e
onkark98 <onkark98@gmail.com>
2024-03-04 17:34:08 +0530

    Changing default TestInstance behavior

--- junit-5-basics/src/test/java/io/javabrains/MathUtilsTest.java
+++ junit-5-basics/src/test/java/io/javabrains/MathUtilsTest.java
@@ -2,11 +2,9 @@ package io.javabrains;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class MathUtilsTest {
 
 	private MathUtils mathUtils;
